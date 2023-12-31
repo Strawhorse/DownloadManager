68ad134fb1aaa7588fc88a1a986c287def280a69
Sean B <strawhorse@gmail.com>
2023-12-31 15:07:13 +0000

    Created main class

--- src/main/java/com/jbr/downloadmanager/DownloadManager.java
+++ src/main/java/com/jbr/downloadmanager/DownloadManager.java
@@ -2,8 +2,10 @@
 
 package com.jbr.downloadmanager;
 
+import javax.swing.JFrame;
 
-public class DownloadManager {
+
+public class DownloadManager extends JFrame {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
